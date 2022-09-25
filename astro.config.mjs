d5d5bed3125af3069ad2e4c9b98caee7786619bf
yKmhr0616 <89152516+yKmhr0616@users.noreply.github.com>
2022-09-26 01:02:48 +0900

    [add] base layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+import tailwind from "@astrojs/tailwind";
+
+import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), tailwind(), vue()],
+  tailwindConfig: "./tailwindConfig.js",
 });
