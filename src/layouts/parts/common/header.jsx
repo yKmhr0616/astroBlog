447a8f5146efda24d5bb3fd7eceee869ec79ed46
yKmhr0616 <89152516+yKmhr0616@users.noreply.github.com>
2023-08-02 02:31:33 +0900

    fix: layout renewal

---
+++ src/layouts/parts/common/header.jsx
@@ -0,0 +1,125 @@
+import { useState } from 'react'
+import { Dialog } from '@headlessui/react'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+
+const navigation = [
+    { name: 'Product', href: '#' },
+    { name: 'About', href: '#' },
+    { name: 'Blog', href: '#' },
+]
+
+export function Header() {
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+
+    return (
+        <header className="bg-white">
+            <nav
+                className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
+                aria-label="Global"
+            >
+                <div className="flex flex-1">
+                    <div className="hidden lg:flex lg:gap-x-12">
+                        {navigation.map((item) => (
+                            <a
+                                key={item.name}
+                                href={item.href}
+                                className="text-sm font-semibold leading-6 text-gray-900"
+                            >
+                                {item.name}
+                            </a>
+                        ))}
+                    </div>
+                    <div className="flex lg:hidden">
+                        <button
+                            type="button"
+                            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+                            onClick={() => setMobileMenuOpen(true)}
+                        >
+                            <span className="sr-only">Open main menu</span>
+                            <Bars3Icon
+                                className="h-6 w-6"
+                                aria-hidden="true"
+                            />
+                        </button>
+                    </div>
+                </div>
+                <div className="flex flex-1 justify-start">
+                    <a
+                        href="/"
+                        className="-translate-x-2/4"
+                    >
+                        <span className="sr-only">AstroBlog</span>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="w-8 h-8"
+                            width="32"
+                            height="32"
+                            viewBox="0 0 256 256"
+                        >
+                            <path
+                                fill="currentColor"
+                                d="M248 128a56 56 0 0 1-95.6 39.6l-.33-.35l-59.95-67.7a40 40 0 1 0 0 56.9l8.52-9.62a8 8 0 1 1 12 10.61l-8.69 9.81l-.33.35a56 56 0 1 1 0-79.2l.33.35l59.95 67.7a40 40 0 1 0 0-56.9l-8.52 9.62a8 8 0 1 1-12-10.61l8.69-9.81l.33-.35A56 56 0 0 1 248 128Z"
+                            />
+                        </svg>
+                    </a>
+                </div>
+            </nav>
+            <Dialog
+                as="div"
+                className="lg:hidden"
+                open={mobileMenuOpen}
+                onClose={setMobileMenuOpen}
+            >
+                <div className="fixed inset-0 z-10" />
+                <Dialog.Panel className="fixed inset-y-0 left-0 z-10 w-full overflow-y-auto bg-white px-6 py-6">
+                    <div className="flex items-center justify-between">
+                        <div className="flex flex-1">
+                            <button
+                                type="button"
+                                className="-m-2.5 rounded-md p-2.5 text-gray-700"
+                                onClick={() => setMobileMenuOpen(false)}
+                            >
+                                <span className="sr-only">Close menu</span>
+                                <XMarkIcon
+                                    className="h-6 w-6"
+                                    aria-hidden="true"
+                                />
+                            </button>
+                        </div>
+                        <div className="flex flex-1 justify-start">
+                            <a
+                                href="/"
+                                className="-translate-x-2/4"
+                            >
+                                <span className="sr-only">Your Company</span>
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="w-8 h-8"
+                                    width="32"
+                                    height="32"
+                                    viewBox="0 0 256 256"
+                                >
+                                    <path
+                                        fill="currentColor"
+                                        d="M248 128a56 56 0 0 1-95.6 39.6l-.33-.35l-59.95-67.7a40 40 0 1 0 0 56.9l8.52-9.62a8 8 0 1 1 12 10.61l-8.69 9.81l-.33.35a56 56 0 1 1 0-79.2l.33.35l59.95 67.7a40 40 0 1 0 0-56.9l-8.52 9.62a8 8 0 1 1-12-10.61l8.69-9.81l.33-.35A56 56 0 0 1 248 128Z"
+                                    />
+                                </svg>
+                            </a>
+                        </div>
+                    </div>
+                    <div className="mt-6 space-y-2">
+                        {navigation.map((item) => (
+                            <a
+                                key={item.name}
+                                href={item.href}
+                                className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                            >
+                                {item.name}
+                            </a>
+                        ))}
+                    </div>
+                </Dialog.Panel>
+            </Dialog>
+        </header>
+    )
+}
